5bb496f0e8e8c8d1629a86db95d3b6f354d3feff
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2025-02-04 16:46:01 +0530

    done

--- DynamicPrograming/01_746_minmum_cost_clinbing.cpp
+++ DynamicPrograming/01_746_minmum_cost_clinbing.cpp
@@ -1,5 +1,5 @@
 #include <vector>
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Solution
@@ -17,9 +17,57 @@ public:
 
         return cost[n] + min(solveR(cost, n - 1), solveR(cost, n - 2));
     }
+    int solveM(vector<int> &cost, int n, vector<int> dp)
+    {
+        if (n == 0)
+        {
+            return cost[0];
+        }
+        if (n == 1)
+        {
+            return cost[1];
+        } // step3
+        if (dp[n] != -1)
+        {
+            return dp[n];
+        }
+
+        dp[n] = cost[n] + min(solveM(cost, n - 1, dp), solveM(cost, n - 2, dp));
+        return dp[n];
+    }
+    int solveT(vector<int> &cost)
+    {
+        int n = cost.size();
+        vector<int> dp(n + 1, -1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+        }
+        return min(dp[n - 2], dp[n - 1]);
+    }
+
+    int solveS(vector<int> &cost)
+    {
+        int n = cost.size();
+        int curr = cost[1];
+        int prev = cost[0];
+        for (int i = 2; i < n; i++)
+        {
+            int temp = cost[i] + min(curr, prev);
+            prev = curr;
+            curr = temp;
+        }
+        return min(curr, prev);
+    }
     int minCostClimbingStairs(vector<int> &cost)
     {
         int n = cost.size();
-        return min(solveR(cost, n - 1), solveR(cost, n - 2));
+        // return min(solveR(cost,n-1),solveR(cost,n-2));
+        // vector<int>dp(n,-1);
+        // return min(solveM(cost,n-1,dp),solveM(cost,n-2,dp));
+        // return solveT(cost);
+        return solveS(cost);
     }
 };

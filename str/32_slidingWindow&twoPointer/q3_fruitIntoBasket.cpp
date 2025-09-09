6d94e19ce8e7f0fd03ea4eaa204ca964c186e2f7
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2025-09-10 02:49:06 +0530

    done

--- str/32_slidingWindow&twoPointer/q3_fruitIntoBasket.cpp
+++ str/32_slidingWindow&twoPointer/q3_fruitIntoBasket.cpp
@@ -1,1 +1,62 @@
-q4
+q4class Solution{
+  public :
+      int totalFruit(vector<int> & fruits){
+          // brute force
+
+          // int ans=0,n=fruits.size(),k=2;
+          //    set<int>st;
+          // for(int i=0;i<=n-1;i++){
+          //     st.clear();
+          //     for(int j=i;j<n;j++){
+          //         if(st.size()>k){
+          //             break;
+          //         }
+          //         st.insert(fruits[j]);
+          //         ans=max(ans,j-i+1);
+
+          //     }
+          // }
+
+          // better
+
+          //     int l=0,r=0,n=fruits.size();
+          //     unordered_map<int,int> mp;
+          // int k=2,ans=0;
+          //     for(r=0;r<n;r++){
+          //         mp[fruits[r]]+=1;
+          //         while(mp.size()>k){
+          //             mp[fruits[l]]--;
+          //             if(mp[fruits[l]]==0){
+          //                 mp.erase(fruits[l]);
+          //             }
+          //             l++;
+          //         }
+
+          //         ans=max(ans,r-l+1);
+          //     }
+          //     return ans;
+          //     }
+
+          ////optimise
+          int l = 0, r = 0, n = fruits.size();
+unordered_map<int, int> mp;
+int k = 2, ans = 0;
+for (r = 0; r < n; r++)
+{
+  mp[fruits[r]]++;
+  if (mp.size() > k)
+  {
+    mp[fruits[l]]--;
+    if (mp[fruits[l]] == 0)
+    {
+      mp.erase(fruits[l]);
+    }
+    l++;
+  }
+
+  ans = max(ans, r - l + 1);
+}
+return ans;
+}
+}
+;
---
+++ str/32_slidingWindow&twoPointer/q4_LongestRepeatingcharacterreplacement.cpp
@@ -0,0 +1,70 @@
+// 424. Longest Repeating Character Replacement
+//     Attempted
+//         Medium
+//             Topics
+//                 premium lock icon
+//                     Companies
+//                         You are given a string s and an integer k.You can choose any character of the string and change it to any other uppercase English character.You can perform this operation at most k times.
+
+//     Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<climits>
+using namespace std;
+
+int characterReplacement(string s,int k){
+  int maxlen=0;
+  int n=s.size();
+  for(int i=0;i<n;i++){
+    unordered_map<int,int>mp;
+    int maxfreq=0;
+    for(int j=i;j<n;j++){
+      char ch=s[j]-'A';
+      mp[ch]++;
+      maxfreq=max(maxfreq,mp[ch]);
+      int len=j-i+1;
+      int rem=len-maxfreq;
+      if(rem<=k){
+        maxlen=max(maxlen,len);
+      }
+    }
+  }
+  return maxlen;
+}
+int characterReplacement(string s,int k){
+  int maxlen=0;
+  int l=0,r=0;
+  unordered_map<int,int>mp;
+int maxfreq=0;
+  while(r<n){
+    char ch=s[r]-'A';
+    mp[ch]++;
+    
+    maxfreq=max(maxfreq,mp[ch]);
+    int len=r-l+1;
+    int rem=len-maxfreq;
+
+    if(rem>k){
+
+
+    }
+  }
+}
+
+int main(){
+  char arrchar[100000];
+  int k;
+  scanf("%s",arrchar);
+
+  scanf("%d",&k);
+  string s(arrchar);
+  printf("%d",characterReplacement(s,k));
+
+
+  return 0;
+
+}
+

342912a85a3e1738c6b78e2c2316aa4251048a1a
HARSHITA <114668133+H-a-r-s-h-i-t-a-k@users.noreply.github.com>
2025-09-14 23:21:18 +0530

    done

--- str/11_Hard_array/05_Largest_subarraywith0sum.cpp
+++ str/11_Hard_array/05_Largest_subarraywith0sum.cpp
@@ -1,1 +1,88 @@
- 
+ #include<iostream>
+ #include<vector>
+ #include<bits/stdc++.h>
+ using namespace std;
+
+
+ int solve(vector<int>& arr,int &n,int &k){
+  int maxlen=0;
+  for(int i=0;i<n;i++){
+    for(int j=i;j<n;j++){
+      int sum=0;
+      for(int k=i;k<=j;k++){
+        sum+=arr[k];
+        if(sum==0){
+          int len=k-i+1;
+          maxlen=max(len,maxlen);
+        }
+      }
+
+    }
+  }
+  return maxlen;
+ }
+
+ int solveBetter(vector<int>&arr,int &n,int&k){
+   int maxlen = 0;
+   for (int i = 0; i < n; i++)
+   {
+     int sum = 0;
+     for (int j = i; j < n; j++)
+     {
+       sum += arr[j];
+       if (sum == 0 && maxlen < j - i + 1)
+       {
+         maxlen = max(maxlen, (j - i + 1));
+       }
+     }
+   }
+   return maxlen;
+ }
+
+ int solveOptimal(vector<int>& arr,int n,int k){
+   unordered_map<int, int> mp;
+   int maxlen = 0;
+   // prefixsum,index
+   int prefixsum = 0;
+   for (int i = 0; i < n; i++)
+   {
+     prefixsum += arr[i];
+
+     if (prefixsum == k)
+     {
+       maxlen = i + 1;
+     }
+     else
+     {
+       if (mp.find(prefixsum) != mp.end())
+       {
+
+         int len = i - mp[prefixsum];
+         maxlen = max(maxlen, len);
+       }
+       else
+       {
+
+         mp[prefixsum] = i;
+       }
+     }
+   }
+
+   return maxlen;
+ }
+ int main(){
+  int n=0;
+  cin>>n;
+  int k=0;
+  cin>>k;
+  vector<int> arr(n);
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+  int ans;
+  //  ans=solve(arr,n,k);
+   ans=solveBetter(arr,n,k);
+  //  ans=solveOptimal(arr,n,k);
+  cout<<ans<<endl;
+  return 0;
+ }
---
+++ str/11_Hard_array/06_count_nOfSubarray_givenxorK.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+int solve(vector<int>&nums,int k){
+  int n=nums.size();
+  int cnt=0;
+  unordered_map<int,int>mp;
+  for(int i=0;i<)
+
+
+
+
+  return cnt;
+}
+
+
+
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> nums;
+  int k=0;
+  cin>>k;
+  for(int i=0;i<n;i++){
+    cin>>nums[i];
+  }
+
+  int ans=solve(nums,k);
+
+
+
+  return 0;
+}
